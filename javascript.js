8c50028008d5d739945c472e2c22c319c40b1250
SeeviewCreator <105053617+SeeviewCreator@users.noreply.github.com>
2023-01-15 16:45:31 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+    document.getElementById("fade").style.display = "none";
+    document.getElementById("popupMail").style.display = "none";
+    document.getElementById("body").style.overflow = "auto";
+}
+var zaehler = 0;
+function popupMail() {
+    if (zaehler == 0){
+        document.getElementById("fade").style.display = "block";
+        document.getElementById("popupMail").style.display = "block";
+        document.getElementById("body").style.overflow = "hidden";
+        zaehler++;
+    } else if (zaehler == 1){
+        document.getElementById("fade").style.display = "none";
+        document.getElementById("popupMail").style.display = "none";
+        document.getElementById("body").style.overflow = "auto";
+        zaehler--;
+    }
+}
